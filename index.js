13aca4c5eec75badd349aca5ddb4e548e91c2885
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-15 23:01:55 +0000

    last  commit

--- index.js
+++ index.js
@@ -1,28 +1,66 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const dotenv = require("dotenv");
+// const userRoute = require("./routes/user");
+// const productRoute = require("./routes/product");
+// const cartRoute = require("./routes/cart");
+// const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+// const authRoute = require("./routes/auth");
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => console.log("DB connection Successfull"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
+// app.use("/api/auth", authRoute);
+
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Backend server is running!");
+// });
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const authRoute = require("./routes/auth");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB connection Successfull"))
+  .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/auth", authRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
