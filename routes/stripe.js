398a0f523c8b8c7ea7477038a62dd883b891a454
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-17 22:47:15 +0000

    shop commerce api  commit

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(
+  "sk_test_51K6eqKIQw5SQ1kyY0eSCVJmJxmtZa4t8TN4wzTPGZLyWiOhyWdDCBZYsvoRfcsESiHxtF5m9plj4bbWEgC5QmjV700kcSjm5Vd"
+);
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
+router.post("/payment", async (req, res) => {
+  await stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
